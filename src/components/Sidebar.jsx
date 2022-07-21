7aef646d151d318d63fbb4cb376592ee012f0c5c
huytd <huytd6627@massgenie.com>
2022-07-21 17:17:58 +0700

    finish NavBar, SideBar update Ecommerce page

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,7 +9,13 @@ import { links } from "../data/dummy";
 import { useStateContext } from "../contexts/ContextProvider";
 
 const Sidebar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+
+  const handleCloseSideBar = () => {
+    if (activeMenu && screenSize <= 900) {
+      setActiveMenu(false);
+    }
+  };
 
   const activeLink =
     "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2 bg-blue-500";
@@ -23,7 +29,7 @@ const Sidebar = () => {
             <Link
               to="/"
               className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold dark:text-white tracking-tighter text-slate-900"
-              onClick={() => setActiveMenu(false)}
+              onClick={handleCloseSideBar}
             >
               <SiShopware />
               <span>Shoppy</span>
@@ -48,7 +54,7 @@ const Sidebar = () => {
                   <NavLink
                     to={`/${link.name}`}
                     key={link.name}
-                    onClick={() => {}}
+                    onClick={handleCloseSideBar}
                     className={({ isActive }) =>
                       isActive ? activeLink : normalLink
                     }
