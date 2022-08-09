2851c5375b68f927786a92c7b6f5b903f0a26be3
huytd <huytd6627@massgenie.com>
2022-08-09 18:07:59 +0700

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
+    <div className="mt-28 mb-10">
+      <p className="text-sm text-center text-gray-500 dark:text-gray-200 m-2">
+        @2022 Duy Huy - Practice ReactJs, Syncfusion, Tailwind
+      </p>
+      <p className="text-xs text-center text-gray-500 dark:text-gray-200 m-2">
+        Reference :{" "}
+        <a href="https://www.youtube.com/watch?v=jx5hdo50a2M">
+          Javascript Mastery Channel
+        </a>
+      </p>
+    </div>
+  );
 }
 
 export default Footer
